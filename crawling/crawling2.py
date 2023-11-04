f3007f6e8c7a4086842a72cbd744f9bedd2fc13a
iriSoon <42645652+iriscuro@users.noreply.github.com>
2023-11-04 18:15:47 -0500

    Update crawling2.py
    Creación de dataset.json

--- crawling/crawling2.py
+++ crawling/crawling2.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from unicodedata import normalize
 import json
 import re
+import time
 
 # Función para extraer el contenido de los párrafos
 def extract_content(soup):
@@ -37,6 +38,8 @@ def scrape_wikipedia(url, max_pages, current_depth=0):
     if current_depth >= max_pages:
         return
 
+    start_time = time.time()  # Registrar el tiempo de inicio
+
     response = requests.get(url)
     if response.status_code == 200:
         page_content = response.text
@@ -51,11 +54,14 @@ def scrape_wikipedia(url, max_pages, current_depth=0):
         # Extraer el snippet (primer párrafo, 200 primeras palabras)
         snippet = " ".join(content_elements[0].text.split()[:200]) if content_elements else ""
         
-         # Definir una expresión regular para filtrar los enlaces no deseados
+        # Definir una expresión regular para filtrar los enlaces no deseados
         pattern = re.compile(r"/wiki/.*:|/wiki/Wiki")
 
         # Extraer los enlaces salientes
         outgoing_links = [a["href"] for a in soup.find_all("a", href=True) if a["href"].startswith("/wiki/") and not pattern.match(a["href"])]
+
+        # Imprimir el número de outgoing_links
+        print(f'Página: {url}, outgoing_links: {len(outgoing_links)}')
 
         # Guardar los datos en un diccionario
         page_data = {
@@ -72,11 +78,15 @@ def scrape_wikipedia(url, max_pages, current_depth=0):
             next_url = f"https://es.wikipedia.org{link}"
             scrape_wikipedia(next_url, max_pages, current_depth + 1)
 
+    end_time = time.time()  # Registrar el tiempo de finalización
+    elapsed_time = end_time - start_time
+    print(f'Tiempo para la página : {elapsed_time:.2f} segundos')
+
 # URL de la página de Wikipedia a la que deseas iniciar el web scraping
-initial_url = "https://es.wikipedia.org/wiki/Wikipedia:Portada"
+initial_url = "https://es.wikipedia.org/wiki/Algoritmo"
 
 # Máximo número de páginas a recopilar
-max_pages = 5000
+max_pages = 2
 
 # Crear una lista para almacenar los datos
 data = []
@@ -85,5 +95,5 @@ data = []
 scrape_wikipedia(initial_url, max_pages)
 
 # Guardar el dataset como un archivo JSON
-with open('wikipedia_dataset.json', 'w', encoding='utf-8') as json_file:
+with open('wikipedia_dataset2.json', 'w', encoding='utf-8') as json_file:
     json.dump(data, json_file, ensure_ascii=False, indent=4)
